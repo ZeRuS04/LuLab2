229bb90634eb3f0ca14977691225dcb9afae3b75
ZeRuS04 <ZeXa05@yandex.ru>
2015-06-04 21:02:09 +0400

    bugfix

--- src/Window.java
+++ src/Window.java
@@ -1,18 +1,15 @@
 import java.util.LinkedList;
 import java.util.Random;
-
 import static java.lang.Thread.sleep;
 
 /**
  * Created by Admi on 28.05.2015.
  */
 public class Window {
-    private int _numberOnDisplay = -1;
+    private Integer _numberOnDisplay = -1;
     private LinkedList<Integer> _numbers;
-
     public Window() {
         _numbers = new LinkedList<>();
-
     }
     public synchronized void getService(){                      // Получение услуги
         Random rand = new Random();
@@ -22,24 +19,26 @@ public class Window {
         }catch (InterruptedException e){
             return;
         }
-
-        if(_numbers.isEmpty())                                  //Если есть еще люди в очереди вызовем
-            _numberOnDisplay = -1;
-        else{
-            System.out.print("Display:" + _numbers.getFirst());
-            System.out.println();
-            _numberOnDisplay = _numbers.poll();
+        synchronized (_numberOnDisplay) {
+            if (_numbers.isEmpty()) {                                  //Если есть еще люди в очереди вызовем
+                _numberOnDisplay = -1;
+            } else {
+                System.out.print("Display:" + _numbers.getFirst());
+                System.out.println();
+                _numberOnDisplay = _numbers.poll();
+            }
         }
     }
     public void addNewNumber(int num){                          // получение от билетной машины информации о еще одном посетителе
-        if(_numberOnDisplay == -1) {
-            System.out.print("Display:" + num);
-            System.out.println();
-            _numberOnDisplay = num;
+        synchronized (_numberOnDisplay) {
+            if (_numberOnDisplay == -1) {
+                System.out.print("Display:" + num);
+                System.out.println();
+                _numberOnDisplay = num;
 
+            } else
+                _numbers.add(num);
         }
-        else
-            _numbers.add(num);
     }
 
     public int Display(){
